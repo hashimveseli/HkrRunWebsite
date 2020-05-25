e9578f3ce11e23705d2906af4066213764f751f5
antonsjostrand <anton.sjostrand@hotmail.se>
2020-05-25 22:09:22 +0200

    Added new attributes to workout script

--- public/js/workout.js
+++ public/js/workout.js
@@ -13,7 +13,10 @@
     innerHtml = '<label>Workout ID: ' + workout.workoutId + '</label><br>';
     innerHtml += '<label>Distance: ' + workout.distance + '</label><br>';
     innerHtml += '<label>Steps: ' + workout.steps + '</label><br>';
-    innerHtml += '<label>Time: ' + workout.time + '</label>';
+    innerHtml += '<label>Time: ' + workout.time + '</label><br>';
+    innerHtml += '<label>Distance performance: ' + workout.distPerformance + '</label><br>';
+    innerHtml += '<label>Steps performance: ' + workout.stepsPerformance + '</label><br>';
+
 
     workoutDiv.innerHTML = innerHtml;
 
